c4fd0c52b03cd9e5fbab4677b322874e4d219e43
111Jessica111 <165466969+111Jessica111@users.noreply.github.com>
2025-03-14 16:30:40 +0800

    退出登录

--- app/src/main/java/com/example/myapplication/fragment/MineFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/MineFragment.java
@@ -1,21 +1,71 @@
 package com.example.myapplication.fragment;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.myapplication.LoginActivity;
 import com.example.myapplication.R;
 
 public class MineFragment extends Fragment {
+
+    private View rootView;
+    private Button btn_finish;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        rootView = inflater.inflate(R.layout.fragment_mine, container, false);
+
+        btn_finish = rootView.findViewById(R.id.btn_finish);
+
+        btn_finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getActivity());
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                dialog.setContentView(R.layout.finish);
+                dialog.show();
+
+                Button btn_out = dialog.findViewById(R.id.btn_out);
+                Button btn_stay = dialog.findViewById(R.id.btn_stay);
+
+                btn_out.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        getActivity().finish();
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+                btn_stay.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
     }
 }
